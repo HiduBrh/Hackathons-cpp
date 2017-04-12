ab8a651081d95f3120906484d0f5d3c3062ae10c
bbr <bbr@affini-tech.com>
2017-04-12 16:43:43 +0200

    feat: création du main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include "Utilisateur.h"
+#include "Application.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Utilisateur utilisateur;
+    Application application;
+
+    utilisateur.set_application(application);
+
+
+    utilisateur.get_application()[0];
+
+    int selection = 0;
+
+    do{
+        std::cout << "---------1 ->Créer Hackaton-------"<< std::endl;
+        std::cout << "--------2 ->Selectionner Hackaton--" << std::endl;
+        std::cout << "-------3 ->Ajouter équipe---------" << std::endl;
+        std::cout << "---------4 ->Ajouter note---------" << std::endl;
+        std::cout << "-----------5 ->Sortir-------------" << std::endl;
+        cin >> selection;
+
+        switch (selection) {
+            case 1:
+                utilisateur.get_application().creer_hackaton();
+                break;
+            case 2:
+                ...
+                break;
+            case 3:
+                ...
+                break;
+            case 4:
+                ...
+                break;
+                ...
+            case 5:
+                cout << "Goodbye.\n";
+                break;
+
+            default:
+                cout<< "Mauvaise entrée.\n";
+        }
+
+
+
+        return 0;
+    }
+    while (selection != 5);
+
+
 }
